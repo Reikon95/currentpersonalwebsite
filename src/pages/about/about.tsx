b0c759c0163caaf8ae7bd75a93eb22e02d4ee785
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-04-12 22:04:58 +0100

    contact form setup

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,9 +1,11 @@
+import Contact from "../contact/contact"
+
 export default function About() {
   return (
     <>
       I'm a full stack engineer (although stronger on the front end), currently
       working remotely for M2A Media.
-      
+      <Contact />
     </>
   )
 }
--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,9 +1,35 @@
+import React from "react"
+import { useForm } from "react-hook-form"
+
+type Inputs = {
+  example: string
+  exampleRequired: string
+}
+
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit = (data) => console.log(data)
+
   return (
     <>
       <h2>Contact Me</h2>
 
-      
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
     </>
   )
 }
