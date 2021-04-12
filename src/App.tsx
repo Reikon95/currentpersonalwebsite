d2ea9e5db58f0dac2534cbe7e7b4344ff8937c6a
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-04-12 19:24:58 +0100

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
+import About from "./pages/about/about"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Cam the dev.</header>
+      <header className="App-header">
+        <div>Hi, I'm Cam!</div>
+        <div>
+          I'm a software engineer from the UK. Welcome to my personal website.
+        </div>
+        <About />
+      </header>
     </div>
   )
 }
---
+++ src/pages/about/about.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <>
+      I'm a full stack engineer (although stronger on the front end), currently
+      working remotely for M2A Media.
+      
+    </>
+  )
+}
